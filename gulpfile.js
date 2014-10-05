0c429942a35990e22fc9e61e7f619d4ec457c977
dickeyxxx <jeff@dickeyxxx.com>
2014-10-05 12:12:06 -0700

    plumber

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@ var gulp = require('gulp')
 var concat = require('gulp-concat')
 var uglify = require('gulp-uglify')
 var ngAnnotate = require('gulp-ng-annotate')
+var plumber = require('gulp-plumber')
 
 gulp.task('build', ['js:build'])
 gulp.task('watch', ['js:watch'])
 
 gulp.task('js:build', function () {
   return gulp.src(['ng/**/module.js', 'ng/**/*.js'])
+    .pipe(plumber())
     .pipe(ngAnnotate())
     .pipe(uglify())
     .pipe(concat('app.js'))
