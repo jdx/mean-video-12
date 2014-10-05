229a0aa6d47a63a310258060e903b39d6b4886ce
dickeyxxx <jeff@dickeyxxx.com>
2014-10-05 11:57:44 -0700

    gulp watching

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,14 @@ var gulp = require('gulp')
 var concat = require('gulp-concat')
 
 gulp.task('build', ['js:build'])
+gulp.task('watch', ['js:watch'])
 
 gulp.task('js:build', function () {
   return gulp.src(['ng/**/module.js', 'ng/**/*.js'])
     .pipe(concat('app.js'))
     .pipe(gulp.dest('.'))
+})
+
+gulp.task('js:watch', ['js:build'], function () {
+  gulp.watch('ng/**/*.js', ['js:build'])
 })
