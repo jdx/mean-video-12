830b7e8fe43f2db5a3d168754661606667b3d5d7
dickeyxxx <jeff@dickeyxxx.com>
2014-10-04 18:16:29 -0700

    added user list

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 angular.module('myapp', [])
+
+angular.module('myapp')
+.controller('GithubCtrl', function ($http, $scope) {
+  $http.get('https://api.github.com/users')
+  .then(function (response) {
+    $scope.users = response.data
+  })
+})
